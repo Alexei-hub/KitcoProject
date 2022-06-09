a8f5f84a16a5ed0a77e9cead46165f2919470151
autodoc <alexeym@pkwteile.de>
2022-06-09 12:57:08 +0300

    adding listener

---
+++ src/main/java/Listener.java
@@ -0,0 +1,13 @@
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.testng.ITestListener;
+import org.testng.ITestResult;
+
+public class Listener implements ITestListener {
+
+    @Override
+    public void onTestStart(ITestResult iTestResult) {
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide().screenshots(true).savePageSource(false));
+    }
+
+}
